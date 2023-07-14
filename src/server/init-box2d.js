da39182ecad65a0ec7d12af45c3186adb5e28da1
Ivan Enzhaev <8observer8@gmail.com>
2023-07-14 18:56:16 +0400

    Add the m letter

--- src/server/init-box2d.js
+++ src/server/init-box2d.js
@@ -5,7 +5,7 @@ Box2DLib({
    * This is the default implementation of locateFile.
    * Modify this logic if your Box2D.wasm lives in a different directory.
    */
-  locateFile: (url, scriptDirectory) => "file:///opt/render/project/src/node_modules/box2d-wasm/dist/umd/Box2D.simd.was"
+  locateFile: (url, scriptDirectory) => "file:///opt/render/project/src/node_modules/box2d-wasm/dist/umd/Box2D.simd.wasm"
 });
 
 export let box2d = null;
